64976b50cd612523f34f1c0aa2882a46d73766cf
LemeshkoNastya <anastasia.lemeshko23@gmail.com>
2020-09-30 15:38:54 +0400

    feat: add functions min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length === 0){
+    return 0;
+  } else {
+    let minNumber = array[0];
+    for (let i = 1; i < array.length; i++) {
+      if (array[i] < minNumber) {
+        minNumber = array[i];
+      }
+    }
+    return minNumber;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length === 0){
+    return 0;
+  } else {
+    let maxNumber = array[0];
+    for (let i = 1; i < array.length; i++) {
+      if (array[i] > maxNumber) {
+        maxNumber = array[i];
+      }
+    }
+    return maxNumber;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length === 0){
+    return 0;
+  } else {
+    let avgNumber = 0;
+    for(let i = 0; i < array.length; i++) {
+      avgNumber += array[i];
+    }
+    avgNumber /= array.length;
+    return avgNumber;
+  }
 }
